cce131b1e0ef7374ed108c9b7ba02f69d443fd12
nenadvasilic <vasilic2013@gmail.com>
2022-04-21 15:46:33 +0200

    final commit

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,20 +1,52 @@
+import { MongoClient, ObjectId } from "mongodb";
+import { Fragment } from "react";
+import Head from "next/head";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
-    <MeetupDetail
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+        /*
       image="https://upload.wikimedia.org/wikipedia/commons/6/6d/Nis_center.JPG"
       title="A First Meetup"
       address="Some Street 5, Some City"
       description="A meetup description"
-    />
+    */
+      />
+    </Fragment>
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://<username>:<password>@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
-    fallback: false,
-    paths: [
+    fallback: "blocking",
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+
+    /*
+    [
       {
         params: {
           meetupId: "m1",
@@ -26,6 +58,7 @@ export async function getStaticPaths() {
         },
       },
     ],
+    */
   };
 }
 
@@ -34,10 +67,30 @@ export async function getStaticProps(context) {
 
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  const client = await MongoClient.connect(
+    "mongodb+srv://<username>:<password>@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
 
   return {
     props: {
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
+      /*
       meetupData: {
         image:
           "https://upload.wikimedia.org/wikipedia/commons/6/6d/Nis_center.JPG",
@@ -46,6 +99,7 @@ export async function getStaticProps(context) {
         address: "Some Street 5, Some City",
         description: "A meetup description",
       },
+      */
     },
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 // our-domain.com (localhost:3000)
-
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+import { Fragment } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
+/*
 const DUMMY_MEETUPS = [
   {
     id: "m1",
@@ -18,33 +21,58 @@ const DUMMY_MEETUPS = [
     description: "This is a second meetup!",
   },
 ];
+*/
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active Reactive meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 }
 
 /*
-
 export async function getServerSideProps(context) {
   const req = context.req;
   const res = context.res;
-
   // fetch data from an API
-
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
   };
 }
-
 */
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://<username>:<password>@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1, // on every 1 second
   };
